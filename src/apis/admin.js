9c374c79ccfb2bcdc8971f786f5159d76b5fa786
tangsu99 <18763952906@163.com>
2025-02-17 15:35:12 +0800

    问卷相关

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -2,5 +2,8 @@ import request from "@/utils/requers"
 
 
 export const getWhitelist = () => request.get('/admin/whitelist')
+export const getQuestion = () => request.get('/admin/AllQuestion')
 export const getResponses = () => request.get('/admin/responses')
 export const getUsers = () => request.get('/admin/users')
+export const addSurvey = (data) => request.post('/admin/addSurvey', JSON.stringify(data))
+export const addQuestion = (data) => request.post('/admin/addQuestion', JSON.stringify(data))
--- src/views/admin/Exam.vue
+++ src/views/admin/Exam.vue
@@ -1,15 +1,36 @@
 <script setup>
-import { getResponses } from '@/apis/admin'
+import { getResponses, getQuestion } from '@/apis/admin'
 import { ref } from 'vue';
 
-const data = ref({})
-
-getResponses().then(res => {
-    data.value = res.data
+const questionData = ref({
+    code: -1,
+    list: [],
+    desc: ''
 })
+const responsesData = ref({
+    code: -1,
+    list: [],
+    desc: ''
+})
+
+getQuestion().then(res => {
+    questionData.value = res.data
+})
+getResponses().then(res => {
+    responsesData.value = res.data
+})
+
+
 </script>
 
 <template>
-    <p>问卷管理</p>
-    <p>{{ data }}</p>
+    <h2>问卷管理</h2>
+    <p><button @click="">新建问卷</button></p>
+    <div>
+        <p v-if="!questionData.list.length">暂无数据</p>
+    </div>
+    <h2>答卷管理</h2>
+    <div>
+        <p v-if="!responsesData.list.length">暂无数据</p>
+    </div>
 </template>
