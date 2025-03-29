60ede08d38f2f3c1767c07149569b385ee53f327
tangsu99 <18763952906@163.com>
2025-03-29 19:53:25 +0800

    修复查询自己提交的答卷时返回结果不对劲

--- src/components/ExamList.vue
+++ src/components/ExamList.vue
@@ -5,7 +5,7 @@
     <ul class="list y-scroll">
       <li v-for="res in responseList" :key="res.id">
         <div class="info">
-          <p>{{ res.type }}试卷（{{ res.isReviewed ? '已通过' : '审核中' }}）</p>
+          <p>{{ res.type }}试卷（{{ res.isReviewed ? res.isReviewed === 1 ? '已通过' : '已拒绝' : '审核中' }}）</p>
           <p>{{ dateFormat(res.responseTime) }}</p>
         </div>
         <MCButton v-if="res.isReviewed" class="button" @click="handleClick(res)">查看成绩</MCButton>
