ae92ab44d299a3b194537865d02094912c939111
BusyPacket <1920909109@qq.com>
2025-04-06 16:19:44 +0800

    修改问卷管理样式

--- src/components/admin/Exam.vue
+++ src/components/admin/Exam.vue
@@ -17,7 +17,6 @@ const _getSurveys = () => {
     surveysData.value = res.data;
   });
 };
-_getSurveys();
 
 const createSurvey = () => {
   addSurvey({
@@ -25,58 +24,88 @@ const createSurvey = () => {
     description: prompt('description'),
   });
 };
+
+const editSurvey = (id) => {
+  flag = true;
+  sid = id;
+};
+
+_getSurveys();
 </script>
 
 <template>
   <EditExam v-if="flag" :sid="sid" @close="flag = false" @flush="_getSurveys"></EditExam>
-  <h1>问卷管理</h1>
-  <p><button @click="createSurvey">新建问卷</button></p>
+  <h1 style="user-select: none">问卷管理</h1>
   <hr />
-  <div>
-    <div class="survey-box" v-for="i in surveysData.list" :key="i.id">
-      <h3 class="title">{{ i.name }}</h3>
-      <h4 class="lite-title">
-        <button
-          @click="
-            flag = true;
-            sid = i.id;
-          "
-        >
-          编辑问卷
-        </button>
-        创建时间: <span>{{ i.createTime }}</span> 问卷状态:
-        <span>{{ i.status }}</span>
-      </h4>
+  <ul class="survey-list">
+    <li class="survey" v-if="!surveysData.list.length">暂无数据</li>
+    <li class="survey" v-for="i in surveysData.list" :key="i.id">
+      <p class="name">
+        {{ i.name }}
+      </p>
       <p class="desc">问卷描述：{{ i.description }}</p>
-    </div>
-    <p v-if="!surveysData.list.length">暂无数据</p>
-  </div>
+      <div class="bot">
+        <div v-show="i.status === 1" class="button mount">已应用</div>
+        <div v-show="i.status === 0" class="button umount">未应用</div>
+        <button type="button" class="button hover edit" @click="editSurvey(i.id)">编辑问卷</button>
+      </div>
+    </li>
+    <button type="button" class="survey hover add" @click="createSurvey">新建问卷</button>
+  </ul>
 </template>
 
 <style scoped>
-.survey-box button {
-  display: inline-block;
-}
-.survey-box {
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 16px 0;
-  margin-top: 10px;
-  background-color: #dcdbdb69;
-}
-.survey-box .title,
-.survey-box .lite-title,
-.survey-box .desc {
-  margin-left: 16px;
-  margin-right: 16px;
-}
-button {
+.button {
   font-size: 16px;
-  padding: 5px;
+  padding: 5px 8px;
   border-radius: 5px;
   background-color: #ccc;
+  user-select: none;
 }
-button:hover {
-  background-color: #888;
+
+.survey-list {
+  width: 100%;
+}
+.survey {
+  position: relative;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 16px;
+  margin-top: 10px;
+  background-color: #dcdbdb69;
+  .name {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  .desc {
+    font-size: 18px;
+    line-height: 1.5em;
+  }
+
+  .bot {
+    display: flex;
+    gap: 8px;
+  }
+  .mount {
+    background-color: #ccffcc;
+    border: 1px solid #ddd;
+  }
+  .umount {
+    background-color: #ccc;
+    border: 1px solid #ddd;
+  }
+  .edit {
+    border: 1px solid #ddd;
+  }
+}
+.hover:hover {
+  background-color: #999;
+}
+.add {
+  width: 100%;
+  height: 60px;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
 }
 </style>
