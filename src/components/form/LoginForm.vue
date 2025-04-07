e32a1006d24f2367472266b53c020565361e72a3
tangsu99 <18763952906@163.com>
2025-04-07 09:51:47 +0800

    fix: 命名错误

--- src/components/form/LoginForm.vue
+++ src/components/form/LoginForm.vue
@@ -16,9 +16,9 @@ const loginForm = ref({
   password: '',
 });
 const sendLogin = () => {
-  openDialog('登陆中');
+  openAlert('登陆中');
   if (!checkPassword(loginForm.value.password)) {
-    openDialog('密码必须包含至少一个大写字母、一个小写字母、一个数字和一个特殊字符，且长度为8-16个字符');
+    openAlert('密码必须包含至少一个大写字母、一个小写字母、一个数字和一个特殊字符，且长度为8-16个字符');
     return;
   }
 
@@ -29,7 +29,7 @@ const sendLogin = () => {
     })
     .then((res) => {
       if (res.data.code === 0) {
-        openDialog('登录成功! 即将跳转...');
+        openAlert('登录成功! 即将跳转...');
         setTimeout(() => {
           // 获取目标页面路径
           const redirect = route.query.redirect;
@@ -41,11 +41,11 @@ const sendLogin = () => {
           }
         }, 2000);
       } else if (res.data.code === 1) {
-        openDialog(res.data.desc);
+        openAlert(res.data.desc);
       }
     })
     .catch((err) => {
-      openDialog('登录错误!');
+      openAlert('登录错误!');
     });
 };
 </script>
