7e1daa6e02dc802cd1b5f53a6f6689f56f9eb08d
BusyPacket <1920909109@qq.com>
2026-06-06 19:01:41 +0800

    chore(ts): support image type

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,3 +5,28 @@ declare module '*.css' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
